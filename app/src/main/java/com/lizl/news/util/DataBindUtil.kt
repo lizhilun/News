10e3a372a97c9f5b1b7348de408a0badc7751262
李志伦 <313701855@qq.com>
2020-05-04 14:50:53 +0800

    每次只加载一张图片

--- app/src/main/java/com/lizl/news/util/DataBindUtil.kt
+++ app/src/main/java/com/lizl/news/util/DataBindUtil.kt
@@ -104,7 +104,7 @@ object DataBindUtil
             // true：忽略标签中的尺寸并触发SIZE_READY回调，false：使用img标签中的宽高尺寸，不触发SIZE_READY回调
             .clickable(true) // 是否可点击，默认只有设置了点击监听才可点击
             .imageClick { imageUrls, position ->
-                PopupUtil.showImageViewerPopup(imageUrls, position)
+                PopupUtil.showImageViewerPopup(imageUrls[position])
             } // 设置图片点击回调
             .urlClick { url -> false } // 设置链接点击回调
             .cache(CacheType.all) // 缓存类型，默认为Cache.ALL（缓存图片和图片大小信息和文本样式信息）
